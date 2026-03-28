0c018420b5fbb28f7fbee24e48474364b252f249
Gunmass <77558634+Gunmass@users.noreply.github.com>
2026-03-27 21:40:43 -0400

    new changes

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-df = pd.read_csv("Traffic_Collisions_280340447332117481.csv")
+df = pd.read_csv("artifacts/collisions_cleaned.csv")
 
-columns_to_del = ["OBJECTID", "ACCIDENTNUM", "ACCIDENT_YEAR", "ACCIDENT_MONTH", "ACCIDENT_DAY", "ACCIDENT_HOUR", "ACCIDENT_MINUTE", "ACCIDENT_SECOND", "XCOORD", "YCOORD", "LONGITUDE", "LATITUDE", "COLLISIONTYPE", "CLASSIFICATIONOFACCIDENT", "IMPACTLOCATION", "INITIALDIRECTIONOFTRAVELONE", "INITIALDIRECTIONOFTRAVELTWO", "INITIALIMPACTTYPE", "INTTRAFFICCONTROL", "LIGHTFORREPORT", "THRULANENO", "NORTHBOUNDDISOBEYCOUNT", "SOUTHBOUNDDISOBEYCOUNT", "PEDESTRIANINVOLVED", "CYCLISTINVOLVED", "MOTORCYCLISTINVOLVED", "ENVIRONMENTCONDITION2", "SELFREPORTED", "LASTEDITEDDATE", "CREATE_BY", "CREATE_DATE", "x", "y"]
+columns_to_del = ["OBJECTID", "ACCIDENTNUM", "ACCIDENT_YEAR", "ACCIDENT_MONTH", "ACCIDENT_DAY", "ACCIDENT_HOUR", "ACCIDENT_MINUTE", "ACCIDENT_SECOND", "XCOORD", "YCOORD", "LONGITUDE", "LATITUDE", "COLLISIONTYPE", "CLASSIFICATIONOFACCIDENT", "IMPACTLOCATION", "INITIALDIRECTIONOFTRAVELONE", "INITIALDIRECTIONOFTRAVELTWO", "INITIALIMPACTTYPE", "INTTRAFFICCONTROL", "LIGHTFORREPORT", "THRULANENO", "NORTHBOUNDDISOBEYCOUNT", "SOUTHBOUNDDISOBEYCOUNT", "PEDESTRIANINVOLVED", "CYCLISTINVOLVED", "MOTORCYCLISTINVOLVED", "ENVIRONMENTCONDITION2", "SELFREPORTED", "LASTEDITEDDATE", "CREATE_BY", "CREATE_DATE", "x", "y", "source_row", "year", "has_valid_coords", "distance_to_zone_m", "XMLIMPORTNOTES"]
 
 df_updated = df.drop(columns=columns_to_del)
 df_updated = df_updated[~df_updated.astype(str).apply(lambda x: x.str.lower()).isin(['other']).any(axis=1)]
 df_updated['TRAFFICCONTROLCONDITION'] = df_updated['TRAFFICCONTROLCONDITION'].replace('unknown', 'Not Applicable')
-df_updated['ACCIDENTDATE'] = pd.to_datetime(df_updated['ACCIDENTDATE'], format='%m/%d/%Y %I:%M:%S %p')
+df_updated['ACCIDENTDATE'] = pd.to_datetime(df_updated['ACCIDENTDATE'], format='mixed', errors='coerce')
+df_updated['zone_id'] = df_updated['zone_id'].astype('category')
+df_updated['zone_area_km2'] = df_updated['zone_area_km2'].astype('float64')
 
 encoder = OneHotEncoder(sparse_output=False)
 encoded = encoder.fit_transform(df_updated[['ACCIDENT_WEEKDAY']])
